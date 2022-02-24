e669ccef60cdc30322cae50e5f4de9615be9808d
enes koçer <eneskocerr41@gmail.com>
2022-02-24 21:07:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/besinler_kitabi/FoodDetailFragment.kt
@@ -0,0 +1,47 @@
+package com.example.besinler_kitabi
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.besinler_kitabi.databinding.FragmentFoodDetailBinding
+
+class FoodDetailFragment : Fragment() {
+
+    private var foodid = 0
+
+    private var _binding : FragmentFoodDetailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentFoodDetailBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            foodid = FoodDetailFragmentArgs.fromBundle(it).foodId
+            println(foodid)
+        }
+
+        binding.goFoodListButton.setOnClickListener{
+            val action = FoodDetailFragmentDirections.actionFoodDetailFragmentToFoodListFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+
+    }
+
+}
